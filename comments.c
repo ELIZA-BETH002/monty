e6bffba21c309429124a820cbfe37b501739b16e
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-25 19:19:21 +0100

    Update comments.c

--- comments.c
+++ comments.c
@@ -1,15 +1,1 @@
-#include "monty.h"
-
-void comments(stack_t **stack, char *line) {
-
-    (void)stack;  // Unused parameter, silence the warning
-
-    char *comment = strchr(line, '#');
-
-    if (comment != NULL) {
-
-        *comment = '\0';  // Remove the comment portion from the line
-
-    }
-
-}
+h
