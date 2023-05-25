6a3aa6bb9de1b6c46af0d257af20ded564df6ad2
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-25 18:01:31 +0100

    pint

--- print.c
+++ print.c
@@ -1,1 +1,18 @@
-t
+#include "monty.h"
+
+void print_stack(stack_t *stack)
+
+{
+
+    while (stack != NULL)
+
+    {
+
+        printf("%d\n", stack->n);
+
+        stack = stack->next;
+
+    }
+
+}
+
