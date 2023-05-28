8350b9925013e5e8717c331e10a220bd380b6b98
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-28 12:33:22 +0100

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,55 +1,247 @@
-#include "monty.h"
+#include "monty.h" 
 
-/**
+  
 
-* f_sub - function that substracts nodes
+ /** 
 
-* @head: double head pointer to the stack
+  * op_sub - subtracts top element of stack from second element of stack 
 
-* @counter: line count
+  * @stack: pointer to the stack 
 
-*
+  * @line_number: line where opcode appears 
 
-* Return: nothing
+  */ 
 
-*/
+ void op_sub(stack_t **stack, unsigned int line_number) 
 
-void f_sub(stack_t **head, unsigned int counter)
+ { 
 
-{
+         stack_t *node1 = *stack, *node2; 
 
-	stack_t *temp;	int sub, nd;
+  
 
-	temp = *head;
+         if ((stack == NULL) || (node1 == NULL)) 
 
-	for (nd = 0; temp != NULL; nd++)
+         { 
 
-		temp = temp->next;
+                 fprintf(stderr, "L%u: can't sub, stack too short\n", 
 
-	if (nd < 2)
+                         line_number); 
 
-	{
+                 exit(EXIT_FAILURE); 
 
-		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+         } 
 
-		fclose(bus.file);
+         node2 = node1->next; 
 
-		free(bus.content);
+         if (node2 == NULL) 
 
-		free_stack(*head);
+         { 
 
-		exit(EXIT_FAILURE);
+                 fprintf(stderr, "L%u: can't sub, stack too short\n", 
 
-	}
+                         line_number); 
 
-	temp = *head;
+                 exit(EXIT_FAILURE); 
 
-	sub = temp->next->n - temp->n;
+         } 
 
-	temp->next->n = sub;
+         node2->n -= node1->n; 
 
-	*head = temp->next;
+         *stack = node2; 
 
-	free(temp);
+         node2->prev = NULL; 
 
-}
+         free(node1); 
+
+ } 
+
+  
+
+ /** 
+
+  * op_mul - multiplies the second and first elements of the stack 
+
+  * @stack: pointer to the stack 
+
+  * @line_number: line where opcode appears 
+
+  */ 
+
+ void op_mul(stack_t **stack, unsigned int line_number) 
+
+ { 
+
+         stack_t *node1 = *stack, *node2; 
+
+  
+
+         if ((stack == NULL) || (node1 == NULL)) 
+
+         { 
+
+                 fprintf(stderr, "L%u: can't mul, stack too short\n", 
+
+                         line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         node2 = node1->next; 
+
+         if (node2 == NULL) 
+
+         { 
+
+                 fprintf(stderr, "L%u: can't mul, stack too short\n", 
+
+                         line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         node2->n *= node1->n; 
+
+         *stack = node2; 
+
+         node2->prev = NULL; 
+
+         free(node1); 
+
+ } 
+
+  
+
+ /** 
+
+  * op_div -  divides the second element of stack by first element of stack 
+
+  * @stack: pointer to the stack 
+
+  * @line_number: line where opcode appears 
+
+  */ 
+
+ void op_div(stack_t **stack, unsigned int line_number) 
+
+ { 
+
+         stack_t *node1 = *stack, *node2; 
+
+  
+
+         if ((stack == NULL) || (node1 == NULL)) 
+
+         { 
+
+                 fprintf(stderr, "L%u: can't div, stack too short\n", 
+
+                         line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         node2 = node1->next; 
+
+         if (node2 == NULL) 
+
+         { 
+
+                 fprintf(stderr, "L%u: can't div, stack too short\n", 
+
+                         line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         if (node1->n == 0) 
+
+         { 
+
+                 fprintf(stderr, "L%u: division by zero\n", line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         node2->n /= node1->n; 
+
+         *stack = node2; 
+
+         node2->prev = NULL; 
+
+         free(node1); 
+
+ } 
+
+  
+
+ /** 
+
+  * op_mod - computes remainder of division of top second element by first 
+
+  * element of stack 
+
+  * @stack: pointer to the stack 
+
+  * @line_number: line where opcode appears 
+
+  */ 
+
+ void op_mod(stack_t **stack, unsigned int line_number) 
+
+ { 
+
+         stack_t *node1 = *stack, *node2; 
+
+  
+
+         if ((stack == NULL) || (node1 == NULL)) 
+
+         { 
+
+                 fprintf(stderr, "L%u: can't mod, stack too short\n", 
+
+                         line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         node2 = node1->next; 
+
+         if (node2 == NULL) 
+
+         { 
+
+                 fprintf(stderr, "L%u: can't mod, stack too short\n", 
+
+                         line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         if (node1->n == 0) 
+
+         { 
+
+                 fprintf(stderr, "L%u: division by zero\n", line_number); 
+
+                 exit(EXIT_FAILURE); 
+
+         } 
+
+         node2->n %= node1->n; 
+
+         *stack = node2; 
+
+         node2->prev = NULL; 
+
+         free(node1); 
+
+ }
